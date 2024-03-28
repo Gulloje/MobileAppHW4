61ead83f2471258ac6a6354e9bf2dca6f4c611f0
JessieGullo <129686666+Gulloje@users.noreply.github.com>
2024-03-28 12:06:42 -0400

    added data class

--- app/src/main/java/com/example/mobileapphw4/MainActivity.kt
+++ app/src/main/java/com/example/mobileapphw4/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.mobileapphw4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private val BASE_URL = "https://app.ticketmaster.com/"
+    private val apiKey = "yL6rMKTtCDSqaZBhQ1FCUHf4z6mO3htG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val eventAPI = retrofit.create(EventDataService::class.java)
+
+        findViewById<Button>(R.id.btnSearch).setOnClickListener {
+            val cityName = findViewById<EditText>(R.id.textCity).text.toString()
+            eventAPI.getEventNameByCity(cityName, apiKey).enqueue(object : Callback<TicketData?> {
+                override fun onResponse(call: Call<TicketData?>, response: Response<TicketData?>) {
+                    Toast.makeText(this@MainActivity, "$response", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "onResponse: $response")
+                    if (response.body() != null) {
+                        Log.d(TAG, "Name ${response.body()!!._embedded.events.get(0).url}")
+                    }
+
+                }
+
+                override fun onFailure(call: Call<TicketData?>, t: Throwable) {
+                    Log.d(TAG, "onFailure: $t")
+                }
+            })
+        }
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/mobileapphw4/TicketData.kt
@@ -0,0 +1,59 @@
+package com.example.mobileapphw4
+
+data class TicketData(
+
+    val _embedded: EmbeddedData
+)
+data class EmbeddedData(
+    val events: List<EventData>
+)
+
+data class EventData(
+    val name: String,
+    val url: String,
+    val type: String,
+    val images:  List<Images>,
+    val dates: Dates,
+    val _embedded: EmbeddedVenue,
+    val priceRanges: List<PriceRangeData>
+)
+data class Images(
+    val width: Int,
+    val height: Int,
+    val url: String
+)
+data class Dates(
+    val start: Start
+)
+
+data class Start(
+    val localDate: String,
+    val localTime: String
+)
+
+data class EmbeddedVenue(
+    val venues: List<VenueData>
+)
+
+data class VenueData(
+    val name: String,
+    //might use postal code here instead
+    val city: City,
+    val state: State,
+    val address: Address
+)
+data class Address(
+    val line1: String
+)
+data class State(
+    val stateCode: String
+)
+data class City(
+    val name: String
+)
+
+data class PriceRangeData(
+    val min: Double,
+    val max: Double,
+    val currency: String
+)
