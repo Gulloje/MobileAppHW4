55786c3c082516266980d9f93007fd74fa6bf7e3
JessieGullo <129686666+Gulloje@users.noreply.github.com>
2024-04-11 12:06:10 -0400

    added firebase stuff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,15 @@ android {
 
 dependencies {
 
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    // Import Firestore
+    implementation("com.google.firebase:firebase-firestore:24.11.0")
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    // FirebaseUI for Firebase Auth
+    implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
     // GSON
---
+++ app/src/main/java/com/example/mobileapphw4/LoginPage.kt
@@ -0,0 +1,70 @@
+package com.example.mobileapphw4
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+
+class LoginPage : AppCompatActivity() {
+    private val TAG = "RegisterActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_page)
+
+        val curUser = FirebaseAuth.getInstance().currentUser
+        //continue if a user already exists
+        if (curUser == null) { //COMEBACK AND CHANGE TO != NULL
+            val intent = Intent(this, MainActivity::class.java)
+            //startActivity(intent)
+            // Make sure to call finish(), otherwise the user would be able to go back to the RegisterActivity
+            //finish()
+        } else {
+            val signActivityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val user = FirebaseAuth.getInstance().currentUser
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    val response = IdpResponse.fromResultIntent(result.data)
+                    if (response == null) {
+                        Log.d(TAG, "onActivityResult: the user has cancelled the sign in request")
+                    } else {
+                        Log.d(TAG, "onActivityResult: ${response.error?.errorCode}")
+                    }
+                }
+            }
+            findViewById<Button>(R.id.btnLogin).setOnClickListener {
+                // Choose authentication providers -- make sure enable them on your firebase account first
+                val providers = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().build()
+                )
+
+                // Create  sign-in intent
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .setAlwaysShowSignInMethodScreen(true) // use this if you have only one provider and really want the see the signin page
+                    .setIsSmartLockEnabled(false)
+                    .build()
+
+                // Launch sign-in Activity with the sign-in intent above
+                signActivityLauncher.launch(signInIntent)
+            }
+        }
+
+    }
+    private fun addUserToFirestore(user: FirebaseUser) {
+
+    }
+
+
+}
